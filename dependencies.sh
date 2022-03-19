8693c14bf1d57f3163a2ead31661adf399eb5b2b
Hakaponttoauto <petri.heinamaki@iki.fi>
2022-03-19 16:33:38 +0200

    add modpackdownloader

---
+++ dependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget https://github.com/Nincraft/ModPackDownloader/releases/download/0.7.1/ModpackDownloader-cli-0.7.1.jar -O modpackdownloader.jar
+wget https://github.com/froehlichA/pax/releases/download/v1.9.8/pax -O pax
---
+++ download-mods.sh
@@ -0,0 +1,2 @@
+rm ./modpack/overrides/mods/*
+java -jar modpackdownloader.jar -manifest ./modpack/manifest.json -folder ./modpack/overrides/mods
